5de5e540934d6eb2d57bf23144aaf68c31e9eb64
julesfiliot <jules.filiot@cpe.fr>
2023-01-06 12:15:54 +0100

    [WIP] navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import lifLogo from '../assets/lif_logo.png';
 import '../styles/components/navbar.scss';
 import BackBtn from './ui/BackBtn';
+import BurgerMenu from './ui/BurgerMenu';
 
 export default function Navbar() {
   return (
     <div className="navbar-container">
       <BackBtn />
       <img src={lifLogo} alt="LIF Logo" className="logo" />
+      <BurgerMenu />
     </div>
   );
 }
--- src/components/ui/BurgerMenu.jsx
+++ src/components/ui/BurgerMenu.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { t } from 'i18next';
+import { Link } from 'react-router-dom';
+import '../../styles/components/ui/burgerMenu.scss';
 
 export default function BurgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <div className="button-container" id="toggle">
-        <span className="top" />
-        <span className="middle" />
-        <span className="bottom" />
+      <div
+        className={`button-container${isOpen ? ' active' : ''}`}
+        onClick={() => setIsOpen(!isOpen)}
+        role="button"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.type === 'keydown' && (e.key === 'Enter' || e.key === ' ')) {
+            setIsOpen(!isOpen);
+          }
+        }}
+      >
+        <div className="top" />
+        <div className="middle" />
+        <div className="bottom" />
       </div>
-      <div className="overlay" id="overlay">
+      <div className={`overlay${isOpen ? ' open' : ''}`}>
         <nav className="overlay-menu">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Work</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
+          <div><Link to="/">{t('navbar.profile')}</Link></div>
+          <div><Link to="/">{t('navbar.achievements')}</Link></div>
+          <div><Link to="/">{t('navbar.discover')}</Link></div>
+          <div><Link to="/">{t('navbar.logout')}</Link></div>
         </nav>
+        {/* <nav className="overlay-menu">
+          <ul>
+            <li><Link to="/">{t('navbar.profile')}</Link></li>
+            <li><Link to="/">{t('navbar.achievements')}</Link></li>
+            <li><Link to="/">{t('navbar.discover')}</Link></li>
+            <li><Link to="/">{t('navbar.logout')}</Link></li>
+          </ul>
+        </nav> */}
       </div>
     </>
   );
