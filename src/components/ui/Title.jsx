b99912c6f1b9932cab90c19e2f8634a51fc11c27
julesfiliot <jules.filiot@cpe.fr>
2023-01-09 09:50:20 +0100

    [ADD] Title component

---
+++ src/components/ui/Title.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../styles/components/ui/Title.scss';
+
+export default function Title({ text, primary, secondary }) {
+  return (
+    <div className={`title-container${primary ? ' primary' : ''}${secondary ? ' secondary' : ''}`}>
+      <h1
+        className="title-h1"
+      >
+        {text}
+      </h1>
+      <div className="underline" />
+    </div>
+  );
+}
+Title.propTypes = {
+  text: PropTypes.string.isRequired,
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+};
+Title.defaultProps = {
+  primary: false,
+  secondary: false,
+};
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import Title from '../components/ui/Title';
 
 function App() {
   const { t } = useTranslation();
@@ -10,10 +11,11 @@ function App() {
     <div
       className="App"
       style={{
-        display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '90vh',
+        display: 'flex', flexDirection: 'column', alignItems: 'center', height: '90vh',
       }}
     >
       <header className="App-header">
+        <Title text="Achievements" />
         <button type="button" onClick={() => navigate('/achievements?page=cat')}>TEST</button>
         <p>{t('test')}</p>
         <p>{t('test')}</p>
